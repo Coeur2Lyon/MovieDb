0a92c3c430cd9d8ca776fce3ea605a0b27e24ece
Vincent <vincent.fillon@groupepsih.com>
2019-11-14 12:01:52 +0100

    chore:suppressions de fichiers tests incorrects

--- src/test/vincentfillon/MainTest.java
+++
@@ -1,4 +0,0 @@
-package test.vincentfillon;
-
-public class MainTest {
-}
--- src/test/vincentfillon/views/MovieEditDialogControllerTest.java
+++
@@ -1,5 +0,0 @@
-package test.vincentfillon.views;
-import junit.framework.TestCase;
-
-public class MovieEditDialogControllerTest extends TestCase {
-}
